e2257b6b6426ceba4cd39064726476387402f5bc
stud238008 <61683903+stud238008@users.noreply.github.com>
2020-12-29 15:18:11 +0100

    Fix gitignore

---
+++ layer/python/lib/python3.7/site-packages/PIL/PixarImagePlugin.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3eb8b22b674617b79c6e6fd9341e9e5a52f90a2540d9dd11918c384d84b49fb6
+size 1647
---
+++ layer/python/lib/python3.7/site-packages/PIL/SunImagePlugin.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:398894d7304194dfe536aa06a7100fb990c3c819b7d40ffe0a4ff0ba5eef22ff
+size 4302
